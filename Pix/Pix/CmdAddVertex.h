5b492f55d2a8e11cbc675cd629ddfd856010b297
lcvtech <itservices@lasallecollegevancouver.com>
2024-05-23 19:21:49 -0700

    Changed AddVertex into Vertex command, added light scripts

--- Pix/Pix/CmdAddVertex.h
+++ Pix/Pix/CmdAddVertex.h
@@ -6,15 +6,15 @@ class CmdAddVertex : public Command
 public:
 	const char* GetName() override
 	{
-		return "AddVertex";
+		return "Vertex";
 	}
 
 	const char* GetDescription() override
 	{
 		return
-			"AddVertex(x, y, z)\n"
-			"AddVertex(x, y, r, g, b)\n"
-			"AddVertex(x, y, z, r, g, b)\n"
+			"Vertex(x, y, z)\n"
+			"Vertex(x, y, r, g, b)\n"
+			"Vertex(x, y, z, r, g, b)\n"
 			"\n"
 			"- Adds vertex to primitives manager before render.";
 	}
